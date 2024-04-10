861b765a18191ed531515c0f3929d835728036dc
sandersonsamuel <sandersonsamuel401@gmail.com>
2024-04-10 02:55:00 -0300

    fix: tentativa de resolver bug de pontuação no mobile

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -16,11 +16,7 @@ export const Header = () =>{
                 <div className={'flex flex-col items-center justify-evenly gap-2'}>
 
                     <p className={'text-4xl font-bold text-center'}>
-                        {sectionCurrent.nome}
-                    </p>
-
-                    <p className={'font-bold text-xl'}>
-                        Pontuação: {snapScore.value.toFixed(2) + sectionCurrent.nome}
+                        {sectionCurrent.nome + snapScore.value.toFixed(2)}
                     </p>
                 </div>
 
